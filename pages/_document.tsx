7f1339a305c14ea556fc424d15534ac832f5c969
Laura <laurabeatriserafim@gmail.com>
2020-07-27 08:56:56 -0300

    chore: add custom document

---
+++ pages/_document.tsx
@@ -0,0 +1,22 @@
+import Document, {
+  Html, Head, Main, NextScript, DocumentProps,
+} from "next/document";
+
+class MyDocument extends Document<DocumentProps> {
+  render(): JSX.Element {
+    return (
+      <Html>
+        <Head>
+          <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
