cc2e90aae65deaf62da4f2101fde228ddab6b39d
Laura <laurabeatriserafim@gmail.com>
2020-07-27 07:54:30 -0300

    style: add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  extends: [
+    "@jungsoft/eslint-config/react-native",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  plugins: [
+    "@typescript-eslint",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  rules: {
+    "react/jsx-filename-extension": [1, { extensions: [".tx", ".tsx"] }],
+    "@typescript-eslint/explicit-function-return-type": [
+      "error",
+      {
+        allowExpressions: true,
+      },
+    ],
+    "no-unused-vars": 0,
+    "@typescript-eslint/no-unused-vars": [2, {
+      argsIgnorePattern: "^_",
+      varsIgnorePattern: "^_",
+    }],
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        ts: "never",
+        tsx: "never",
+      },
+    ],
+    "react/react-in-jsx-scope": 0,
+    "react/jsx-props-no-spreading": 0,
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {},
+    },
+  },
+};
