c4efff82e8639ffbfaa4c46807ca340de3ca781d
Laura <laurabeatriserafim@gmail.com>
2020-07-27 07:44:42 -0300

    chore: configure root app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,28 @@
 import { NextPage } from "next";
 import { AppProps } from "next/app";
-import Head from "next/head";
+import { Global, css } from "@emotion/core";
+import { ColorModeProvider, CSSReset, ThemeProvider } from "@chakra-ui/core";
+import { DefaultSeo } from "next-seo";
+
+import seo from "../config/seo";
+
+const rootFullHeight = css`
+  #__next {
+    min-height: 100%;
+  }
+`;
 
 const App: NextPage<AppProps> = ({ Component, pageProps }) => (
-  <>
-    <Head>
-      <title>Recipes</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <Component {...pageProps} />
-  </>
+  <ColorModeProvider value="dark">
+    <ThemeProvider>
+      <CSSReset />
+      <Global
+        styles={rootFullHeight}
+      />
+      <DefaultSeo {...seo}/>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  </ColorModeProvider>
 );
 
 export default App;
