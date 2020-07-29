acf38985a22ac54ad91da54bf070f7e2fb4ee1d1
Laura <laurabeatriserafim@gmail.com>
2020-07-29 07:06:50 -0300

    chore: apply section to home and redirect to recipes page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
+import Link from "next/link";
 import {
-  Text,
   Flex,
   Button,
   Box,
@@ -8,31 +8,11 @@ import {
   Badge,
 } from "@chakra-ui/core";
 
-const Home: React.FC = () => (
-  <Flex
-    backgroundColor="white"
-    flexDirection="column"
-    borderRadius={4}
-    boxShadow="md"
-    maxWidth={400}
-    height={["100%", 812]}
-    marginX="auto"
-    marginTop={[0, 4]}
-  >
-    <Flex
-      justifyContent="space-between"
-      alignItems="center"
-      padding={5}
-      width="100%"
-    >
-      <Text
-        fontWeight="medium"
-        fontSize="3xl"
-        color="gray.600"
-      >
-        Recipes
-      </Text>
+import Section from "components/Section";
 
+const Home: React.FC = () => {
+  const HeaderButton = (
+    <Link href="/recipes/create">
       <Button
         leftIcon="small-add"
         outline={0}
@@ -42,96 +22,100 @@ const Home: React.FC = () => (
       >
         Add
       </Button>
-    </Flex>
+    </Link>
+  );
 
-    <Flex
-      marginTop={10}
-      direction="column"
-      overflow="auto"
-      flexGrow={1}
-    >
-      <Box
-        size="sm"
-        width="90%"
-        maxWidth={233}
-        marginX="auto"
-        height={220}
-        position="relative"
-        marginBottom={4}
+  return (
+    <Section title="Recipes" headerButton={HeaderButton}>
+      <Flex
+        direction="column"
+        overflow="auto"
+        flexGrow={1}
+        paddingY={8}
       >
-        <Image
-          borderRadius={6}
-          objectFit="cover"
+        <Box
+          size="sm"
+          width="90%"
+          maxWidth={233}
           marginX="auto"
-          height="100%"
-          width="100%"
-          src="https://img.estadao.com.br/thumbs/640/resources/jpg/8/9/1569458498298.jpg"
-          alt="Segun Adebayo"
-        />
-        <Badge
-          position="absolute"
-          bottom={2}
-          left={2}
+          height={220}
+          position="relative"
+          marginBottom={4}
         >
-          BBQ Meatloaf
-        </Badge>
-      </Box>
+          <Image
+            borderRadius={6}
+            objectFit="cover"
+            marginX="auto"
+            height="100%"
+            width="100%"
+            src="https://img.estadao.com.br/thumbs/640/resources/jpg/8/9/1569458498298.jpg"
+            alt="Segun Adebayo"
+          />
+          <Badge
+            position="absolute"
+            bottom={2}
+            left={2}
+          >
+            BBQ Meatloaf
+          </Badge>
+        </Box>
 
-      <Box
-        size="sm"
-        width="90%"
-        maxWidth={233}
-        marginX="auto"
-        height={220}
-        position="relative"
-        marginBottom={4}
-      >
-        <Image
-          borderRadius={6}
-          objectFit="cover"
+        <Box
+          size="sm"
+          width="90%"
+          maxWidth={233}
           marginX="auto"
-          height="100%"
-          width="100%"
-          src="https://img.estadao.com.br/thumbs/640/resources/jpg/8/9/1569458498298.jpg"
-          alt="Segun Adebayo"
-        />
-        <Badge
-          position="absolute"
-          bottom={2}
-          left={2}
+          height={220}
+          position="relative"
+          marginBottom={4}
         >
-          BBQ Meatloaf
-        </Badge>
-      </Box>
+          <Image
+            borderRadius={6}
+            objectFit="cover"
+            marginX="auto"
+            height="100%"
+            width="100%"
+            src="https://img.estadao.com.br/thumbs/640/resources/jpg/8/9/1569458498298.jpg"
+            alt="Segun Adebayo"
+          />
+          <Badge
+            position="absolute"
+            bottom={2}
+            left={2}
+          >
+            BBQ Meatloaf
+          </Badge>
+        </Box>
 
-      <Box
-        size="sm"
-        width="90%"
-        maxWidth={233}
-        marginX="auto"
-        height={220}
-        position="relative"
-        marginBottom={4}
-      >
-        <Image
-          borderRadius={6}
-          objectFit="cover"
+        <Box
+          size="sm"
+          width="90%"
+          maxWidth={233}
           marginX="auto"
-          height="100%"
-          width="100%"
-          src="https://img.estadao.com.br/thumbs/640/resources/jpg/8/9/1569458498298.jpg"
-          alt="Segun Adebayo"
-        />
-        <Badge
-          position="absolute"
-          bottom={2}
-          left={2}
+          height={220}
+          position="relative"
+          marginBottom={4}
         >
-          BBQ Meatloaf
-        </Badge>
-      </Box>
-    </Flex>
-  </Flex>
-);
+          <Image
+            borderRadius={6}
+            objectFit="cover"
+            marginX="auto"
+            height="100%"
+            width="100%"
+            src="https://img.estadao.com.br/thumbs/640/resources/jpg/8/9/1569458498298.jpg"
+            alt="Segun Adebayo"
+          />
+          <Badge
+            position="absolute"
+            bottom={2}
+            left={2}
+          >
+            BBQ Meatloaf
+          </Badge>
+        </Box>
+      </Flex>
+    </Section>
+  );
+};
 
 export default Home;
