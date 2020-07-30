4e6e68b2e2863e3e0dc237c884f3e070f4a04e8e
Laura <laurabeatriserafim@gmail.com>
2020-07-30 08:47:29 -0300

    chore: add list recipes query

---
+++ graphql/queries/listRecipes.ts
@@ -0,0 +1,16 @@
+import gql from "graphql-tag";
+
+const LIST_RECIPES_QUERY = gql`
+  query listRecipes {
+    recipes_recipes {
+      id
+      image_url
+      ingredients
+      instructions
+      name
+      recipe_url
+    }
+  }
+`;
+
+export default LIST_RECIPES_QUERY;
