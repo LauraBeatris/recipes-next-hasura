dd0f9a4f1f16f0790957f3ca51719e2d92d79f55
Laura <laurabeatriserafim@gmail.com>
2020-08-02 09:46:43 -0300

    chore: add recipe form component

---
+++ components/RecipeForm/index.tsx
@@ -0,0 +1,92 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers";
+import {
+  Button,
+  Stack,
+} from "@chakra-ui/core";
+
+import Input from "components/Input";
+import TextArea from "components/TextArea";
+import { RecipeFormData } from "types/recipes";
+import createRecipeSchema from "schemas/createRecipe";
+
+interface CreateRecipeForm {
+  onSubmit: (recipeData: RecipeFormData) => void;
+}
+
+const RecipeForm: React.FC<CreateRecipeForm> = ({ onSubmit }) => {
+  const {
+    register,
+    handleSubmit,
+    errors,
+    formState,
+  } = useForm<RecipeFormData>({
+    mode: "all",
+    resolver: yupResolver(createRecipeSchema),
+  });
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Stack>
+        <Input
+          name="name"
+          label="Name"
+          placeholder="Empadão de Camarão"
+          ref={register}
+          error={errors?.name?.message}
+        />
+
+        <Input
+          name="imageUrl"
+          label="Image URL"
+          placeholder="https://www.budgetbads/2018/01231"
+          ref={register}
+          error={errors?.imageUrl?.message}
+        />
+
+        <TextArea
+          name="ingredients"
+          label="Ingredients"
+          placeholder="Butter and salt"
+          ref={register}
+          error={errors?.ingredients?.message}
+        />
+
+        <TextArea
+          name="instructions"
+          label="Instructions"
+          placeholder="Mix em' up"
+          ref={register}
+          error={errors?.instructions?.message}
+        />
+
+        <Input
+          name="recipeUrl"
+          label="Original Recipe URL"
+          placeholder="www.tudogostoso/empadao-de-camarao"
+          ref={register}
+          error={errors?.recipeUrl?.message}
+        />
+      </Stack>
+
+      <Button
+        backgroundColor="blue.400"
+        justifyContent="center"
+        alignItems="center"
+        marginLeft="auto"
+        marginTop="auto"
+        variant="unstyled"
+        display="flex"
+        width="20%"
+        color="white"
+        type="submit"
+        isDisabled={!formState.isValid}
+      >
+        Save
+      </Button>
+    </form>
+  );
+};
+
+export default RecipeForm;
--- pages/recipes/create.tsx
+++ pages/recipes/create.tsx
@@ -1,23 +1,15 @@
 import React from "react";
 import { useMutation } from "@apollo/react-hooks";
-import { useForm } from "react-hook-form";
 import { useRouter } from "next/router";
-import { yupResolver } from "@hookform/resolvers";
 import Link from "next/link";
-import {
-  useToast,
-  Button,
-  Stack,
-} from "@chakra-ui/core";
+import { useToast, Button } from "@chakra-ui/core";
 
 import CREATE_RECIPE_MUTATION from "graphql/mutations/createRecipe";
 import Section from "components/Section";
-import Input from "components/Input";
-import TextArea from "components/TextArea";
 import { RecipeFormData } from "types/recipes";
 import { ROOT_PAGE_PATH } from "constants/router";
-import createRecipeSchema from "schemas/createRecipe";
 import LIST_RECIPES_QUERY from "graphql/queries/listRecipes";
+import RecipeForm from "components/RecipeForm";
 
 const HeaderButton = (
   <Link href="/">
@@ -42,20 +34,10 @@ const CreateRecipe: React.FC = () => {
     ],
   });
 
-  const {
-    register,
-    handleSubmit,
-    errors,
-    formState,
-  } = useForm<RecipeFormData>({
-    mode: "all",
-    resolver: yupResolver(createRecipeSchema),
-  });
-
   const toast = useToast();
   const router = useRouter();
 
-  const onSubmit = handleSubmit((recipeData): void => {
+  const onSubmit = ((recipeData: RecipeFormData): void => {
     createRecipe({
       variables: {
         recipeData,
@@ -83,65 +65,7 @@ const CreateRecipe: React.FC = () => {
 
   return (
     <Section title="Create Recipe" headerButton={HeaderButton}>
-      <form onSubmit={onSubmit}>
-        <Stack>
-          <Input
-            name="name"
-            label="Name"
-            placeholder="Empadão de Camarão"
-            ref={register}
-            error={errors?.name?.message}
-          />
-
-          <Input
-            name="imageUrl"
-            label="Image URL"
-            placeholder="https://www.budgetbads/2018/01231"
-            ref={register}
-            error={errors?.imageUrl?.message}
-          />
-
-          <TextArea
-            name="ingredients"
-            label="Ingredients"
-            placeholder="Butter and salt"
-            ref={register}
-            error={errors?.ingredients?.message}
-          />
-
-          <TextArea
-            name="instructions"
-            label="Instructions"
-            placeholder="Mix em' up"
-            ref={register}
-            error={errors?.instructions?.message}
-          />
-
-          <Input
-            name="recipeUrl"
-            label="Original Recipe URL"
-            placeholder="www.tudogostoso/empadao-de-camarao"
-            ref={register}
-            error={errors?.recipeUrl?.message}
-          />
-        </Stack>
-
-        <Button
-          backgroundColor="blue.400"
-          justifyContent="center"
-          alignItems="center"
-          marginLeft="auto"
-          marginTop="auto"
-          variant="unstyled"
-          display="flex"
-          width="20%"
-          color="white"
-          type="submit"
-          isDisabled={!formState.isValid}
-        >
-          Save
-        </Button>
-      </form>
+      <RecipeForm onSubmit={onSubmit} />
     </Section>
   );
 };
