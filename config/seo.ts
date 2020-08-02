71f77becbc9949a203054d66ed755ec03e763944
Laura <laurabeatriserafim@gmail.com>
2020-08-02 17:06:00 -0300

    chore: update seo url

--- config/seo.ts
+++ config/seo.ts
@@ -2,12 +2,12 @@ const SEO = {
   title: "Recipes | Explore the best recipes",
   description: "Explore the best recipes.",
   openGraph: {
-    // url: 'https://todo.io',
+    url: "recipes-next-hasura.vercel.app",
     title: "Recipes | Explore the best recipes",
     description: "Explore the best recipes.",
     images: [
       {
-        url: "https://daydrink.io/images/banner.png",
+        url: "recipes-next-hasura.vercel.app",
         width: 1200,
         height: 630,
         alt: "Recipes | Explore the best recipes",
