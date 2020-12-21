c334c4edd0fbf2dea4f4276f86bc3d9918a95b2e
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2020-12-21 09:13:50 -0300

    chore: improve type definitions of recipes

--- types/recipes.ts
+++ types/recipes.ts
@@ -1,7 +1,12 @@
-export interface RecipeFormData {
+export interface Recipe {
+  id: string;
   name: string;
   image_url: string;
+  created_at: string;
+  updated_at: string;
   recipe_url: string;
   ingredients: string;
   instructions: string;
 }
+
+export type RecipeFormData = Omit<Recipe, "id" | "created_at" | "updated_at">;
